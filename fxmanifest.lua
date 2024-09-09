e7039381a01e7bb08d1d77def1ead84725589eb3
vipex <101529155+vipexv@users.noreply.github.com>
2024-09-09 19:41:08 +0200

    ci: set ui page to the compiled code instead of localhost

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -20,8 +20,8 @@ server_scripts {
     "server/main.lua",
 }
 
--- ui_page 'dist/web/index.html'
-ui_page 'http://localhost:5173/'
+ui_page 'dist/web/index.html'
+-- ui_page 'http://localhost:5173/'
 
 files {
     'dist/web/index.html',
