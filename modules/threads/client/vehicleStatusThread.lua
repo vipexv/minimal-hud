8563513bcdcc5dd7751759b60407b0a916a51386
vipex <101529155+vipexv@users.noreply.github.com>
2024-09-09 02:12:57 +0200

    fix: unnecessary require and accessing functions multiple times instead of setting a var to access it from

--- modules/threads/client/vehicleStatusThread.lua
+++ modules/threads/client/vehicleStatusThread.lua
@@ -1,6 +1,6 @@
-local debug = require("modules.utils.shared").debug
 local interface = require("modules.interface.client")
 local utility = require("modules.utils.shared")
+local debug = utility.debug
 
 local VehicleStatusThread = {}
 VehicleStatusThread.__index = VehicleStatusThread
@@ -14,6 +14,8 @@ end
 function VehicleStatusThread:start()
   CreateThread(function()
     local ped = PlayerPedId()
+    local playerStatusThread = self.playerStatus
+    local convertRpmToPercentage = utility.convertRpmToPercentage
 
     self.playerStatus:setIsVehicleThreadRunning(true)
 
@@ -21,7 +23,7 @@ function VehicleStatusThread:start()
       local vehicle = GetVehiclePedIsIn(ped, false)
       local engineHealth = tonumber(GetVehicleEngineHealth(vehicle) / 100 * 100)
       local speed = math.floor(GetEntitySpeed(vehicle) * 2.236936)
-      local rpm = utility.convertRpmToPercentage(GetVehicleCurrentRpm(vehicle))
+      local rpm = convertRpmToPercentage(GetVehicleCurrentRpm(vehicle))
       local fuelValue = Entity(vehicle).state.fuel or GetVehicleFuelLevel(vehicle)
       local fuel = math.floor(fuelValue * 10 + 0.5) / 10
       local gears = GetVehicleHighGear(vehicle)
@@ -37,7 +39,7 @@ function VehicleStatusThread:start()
       Wait(120)
     end
 
-    self.playerStatus:setIsVehicleThreadRunning(false)
+    playerStatusThread:setIsVehicleThreadRunning(false)
     debug("(vehicleStatusThread) Vehicle status thread ended.")
   end)
 end
