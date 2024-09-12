46e072a5635377387935fcdad7da77ab44d08c5e
vipex <101529155+vipexv@users.noreply.github.com>
2024-09-12 15:37:46 +0200

    ci: add new export so people can toggle the hud

--- client/main.lua
+++ client/main.lua
@@ -4,11 +4,18 @@ local playerStatusClass = require("modules.threads.client.playerStatus")
 local vehicleStatusClass = require("modules.threads.client.vehicleStatusThread")
 local seatbeltLogicClass = require("modules.seatbelt.client")
 local utility = require("modules.utils.shared")
+local interface = require("modules.interface.client")
+local debug = utility.debug
 
 local seatbeltLogic = seatbeltLogicClass.new()
 local playerStatusThread = playerStatusClass.new("main")
 local vehicleStatusThread = vehicleStatusClass.new(playerStatusThread, seatbeltLogic)
 local framework = utility.isFrameworkValid() and require("modules.frameworks." .. config.framework:lower()).new() or
-false
+    false
 
 playerStatusThread:start(vehicleStatusThread, seatbeltLogic, framework)
+
+exports("toggleHud", function(state)
+  interface.toggle(state or nil)
+  debug("(exports:toggleHud) Toggled HUD to state: ", state)
+end)
