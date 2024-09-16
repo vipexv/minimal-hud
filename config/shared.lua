a13841fa1f5b379a1f2a7ba4f3c89a054c501fb7
vipex <101529155+vipexv@users.noreply.github.com>
2024-09-16 16:22:44 +0200

    ci: set debug mode to false by default

--- config/shared.lua
+++ config/shared.lua
@@ -1,5 +1,5 @@
 return {
-	debug = true,
+	debug = false,
 	useBuiltInSeatbeltLogic = true, -- Whether to enable the custom seatbelt logic in this script, or use your own one, keep in mind that if you decide to use your own seatbelt logic, you will need to edit the source code to tailor your needs.
 	framework = "none", -- none, esx, qb, ox, custom. If you wanna enable the hunger/thirst/stress stats you need to use a framework, or you can edit the source code to tailor your  own custom framework
 }
